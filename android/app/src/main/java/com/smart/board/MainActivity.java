86a659a88dcce9af74902714e1e0eb90047e05f7
Kaushal Krishna <kaushalkrishna011@gmail.com>
2025-11-08 18:37:01 +0000

    FIX: Used non-secure channel to solve local ws connection issues

--- android/app/src/main/java/com/smart/board/MainActivity.java
+++ android/app/src/main/java/com/smart/board/MainActivity.java
@@ -1,5 +1,19 @@
 package com.smart.board;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    try {
+      getBridge().getWebView().getSettings()
+        .setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+    } catch (Exception e) {
+      // ignore: silently fail
+    }
+  }
+}
